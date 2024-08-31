1c2edf679822baf71eae6541f56a049182bbd7db
Artyom <adavydovvv@mail.ru>
2024-08-31 20:44:34 +0300

    Homework 5

---
+++ audit-starter/src/test/java/ru/ylab/auditstarter/AuditStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ylab.auditstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
